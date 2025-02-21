3285b79a9ab2b8d90faaa18cc40ab1f071256780
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-21 22:30:16 +0530

    removed

--- infra-helm-charts/ingress.sh
+++
@@ -1,8 +0,0 @@
-# install ingress
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
-
-#// to connect NGINX ingress to argocd-server
-kubectl create ns argocd
-# create a pod argocd-dev
-kubectl apply -f argocd-dev.yml -n argocd
