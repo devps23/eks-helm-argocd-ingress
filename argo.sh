26ccee26b2ddad2b6369ec918e9818004ea399af
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-25 15:50:44 +0530

    dockerfile

--- argo.sh
+++ argo.sh
@@ -13,11 +13,13 @@ fi
 if [ "$1" == "jobs" ]; then
     argocd login $( kubectl get ingress -A | grep argocd | awk '{print $4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
     for app in backend frontend ; do
-    argocd app create ${app} --repo https://github.com/devps23/eks-helm-argocd-ingress.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yml
+    argocd app create ${app} --repo https://github.com/devps23/eks-helm-argocd-ingress.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --insecure --skip-test-tls --grpc-web --values values/${app}.yml
     done
 fi
 
- argocd app create ${app} --repo https://github.com/devps23/eks-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yaml
+# argocd app create ${app} --repo https://github.com/devps23/eks-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yaml
+#
+#
+# argocd app create backend --repo https://github.com/devps23/eks-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/backend.yaml
+#
 
-
- argocd app create backend --repo https://github.com/devps23/eks-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/backend.yaml
