61312c4f34593a304b990bbde25f6adfba9f29e0
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-08 18:16:00 +0530

    argosh

--- argo.sh
+++ argo.sh
@@ -23,3 +23,4 @@ fi
 # argocd app create backend --repo https://github.com/devps23/eks-helm-argocd.git --path chart --upsert --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/backend.yaml
 #
 
+argocd login argocd-ingress.pdevops78.online --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
