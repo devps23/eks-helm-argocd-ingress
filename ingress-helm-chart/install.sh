cd3d84c7c5cb248f560141ee0789535299557570
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-13 19:53:26 +0530

    removed

--- ingress-helm-chart/install.sh
+++ ingress-helm-chart/install.sh
@@ -1,5 +1,7 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress-values.yaml
+helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 kubectl create namespace -n argocd
+
+kubectl apply -f argocd-ingress-dev.yaml
 
 
