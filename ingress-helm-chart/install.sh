04da91abafa4b5c7d6b39fc47afc84b8d82a9697
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-03-14 12:47:08 +0530

    ng ingress

--- ingress-helm-chart/install.sh
+++ ingress-helm-chart/install.sh
@@ -1,7 +1,7 @@
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+# service for nlb
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 kubectl create namespace -n argocd
-
-kubectl apply -f argocd-ingress-dev.yaml
+kubectl apply -f argocd-ingress-dev.yaml -n argocd
 
 
